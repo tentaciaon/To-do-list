6feb652548ef22e942111a140f55996c830627a6
tentaciaon <21bhaveshpanse@gmail.com>
2024-12-06 21:14:58 +0530

    first commit

---
+++ todo_project/urls.py
@@ -0,0 +1,8 @@
+# todo_list/todo_project/urls.py
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("todo_app.urls"))
+]
